5ff3b984429da9401a8d9a1baeccf054ffcad0fa
Marco Christiani <mchristiani2017@gmail.com>
2024-03-08 17:08:16 -0500

    neuron and mem

--- src/neuron.zig
+++ src/neuron.zig
@@ -16,10 +16,12 @@ pub const Neuron = struct {
         const weights: []*Value = try allocator.alloc(*Value, input_dim);
 
         for (0.., weights) |i, _| {
-            weights[i] = try Value.init(allocator, random.random().float(f64), null);
+            const label = try std.fmt.allocPrint(allocator.*, "neuron-w-{}", .{i});
+            defer allocator.free(label);
+            weights[i] = try Value.init(allocator, random.random().float(f64), label);
         }
 
-        const bias = try Value.init(allocator, 0.0, null);
+        const bias = try Value.init(allocator, 0.0, "neuron-bias");
         const self = try allocator.create(Neuron);
         self.* = Neuron{
             .weights = weights,
@@ -33,59 +35,99 @@ pub const Neuron = struct {
         // operations modify the computation graph which need to outlive this function, so use pointers
         var outputs = try allocator.alloc(*Value, inputs.len);
         for (inputs, 0..) |input, i| {
-            outputs[i] = try add(self.alloc, try mul(self.alloc, input, self.weights[i]), self.bias);
+            outputs[i] = add(self.alloc, mul(self.alloc, input, self.weights[i]), self.bias);
         }
         return outputs;
     }
 
-    // TODO: deinit(): see comments below why this wont work as expected
     pub fn deinit(self: *Self) void {
-        // should tear down the entire graph by cascading from the tail
-        self.weights[self.weights.len - 1].deinit();
+        for (self.weights) |w| {
+            w.deinit();
+        }
         self.alloc.free(self.weights);
-        self.alloc.destroy(self.bias);
+        self.bias.deinit();
+        self.alloc.destroy(self);
+    }
+
+    pub fn detach(self: *Self) void {
+        for (self.weights) |w| {
+            w.detach();
+        }
+        self.bias.detach();
+    }
+    pub fn attach(self: *Self) void {
+        for (self.weights) |w| {
+            w.detached = false;
+        }
+        self.bias.detached = false;
+    }
+
+    pub fn print(self: *Self) void {
+        for (0.., self.weights) |i, w| {
+            std.debug.print("weight {}\n", .{i});
+            w.print();
+        }
+        std.debug.print("bias\n", .{});
+        self.bias.print();
+    }
+
+    pub fn update(self: *Self, lr: f32) void {
+        for (self.weights) |w| {
+            w.value -= lr * w.grad;
+        }
+        self.bias.value -= lr * self.bias.grad;
     }
 };
 
 test "neuron 1->1" {
     const allocator = std.testing.allocator;
+    // const allocator = std.heap.page_allocator;
     const input_size = 1;
-    const n_epochs = 10;
+    const n_epochs = 5;
     const learning_rate = 0.01;
-    _ = learning_rate;
 
     var neuron = try Neuron.init(&allocator, input_size, 42);
 
-    // const inputs = [_]f64{ 1, 2, 3, 4, 5 };
-    const targets = [_]f64{ 1, 4, 9, 16, 25 };
-    // _ = targets;
-    const inputs = [_]f64{1};
+    const inputs = [_]f64{ 1, 2, 3, 4, 5 };
+    const targets = [_]f64{ 3, 5, 7, 9, 11 };
+    // const inputs = [_]f64{1};
 
     for (0..n_epochs) |epoch| {
         for (0.., inputs) |i, _| {
-            const input_value = try Value.init(&allocator, inputs[i], null);
+            neuron.attach();
+            const input_value = try Value.init(&allocator, inputs[i], "input-");
             var inputs_i = [_]*Value{input_value};
 
-            const target_value = try Value.init(&allocator, targets[i], null);
+            const target_value = try Value.init(&allocator, targets[i], "target-");
             var target_i = [_]*Value{target_value};
 
-            // NOTE: outputs have to be free'd but need to live until backprop
-            // similar to linearModel() we can just deinit() loss to tear down the graph?
+            // inputs_i[0].print();
+            // target_i[0].print();
+
             const output = try neuron.forward(&allocator, &inputs_i);
 
-            // TODO: learning
-            const loss = try grad.loss_mse(&allocator, output, &target_i);
+            // std.debug.print("\nGraph 1: {s}\n", .{"{"});
+            // output[0].print_arrows();
+            // std.debug.print("{s}\n", .{"}"});
 
-            // NOTE: This will destroy the weights in the Neuron
-            // Assume what needs to happen is we break the graph after backprop to destroy all intermediate
-            // values, but preserve the Neuron's values (or, could possible use the `label` field as a flag)
+            const loss = try grad.loss_mse(&allocator, output, &target_i);
+            defer allocator.free(output);
             defer loss.deinit();
-            // neuron.backward(loss);
-            // neuron.zeroGrad();
-            std.debug.print("i: {}, output: {}\n", .{ i, output[0].value });
+            try loss.backward();
+            neuron.update(learning_rate);
+            try loss.zero_grad();
+
+            neuron.detach();
+
+            // std.debug.print("Graph 2: {s}\n", .{"{"});
+            // loss.print_arrows();
+            // std.debug.print("{s}\n", .{"}"});
+
+            std.debug.print("i: {}, loss: {}\n", .{ i, loss.value });
         }
 
         std.debug.print("Epoch: {}\n", .{epoch});
     }
+    neuron.print();
     neuron.deinit();
 }
