c13d61cd168c9ec53d60504566013e8329d74ba9
Marco Christiani <mchristiani2017@gmail.com>
2024-07-05 15:17:21 -0400

    ✨ (winit.zig): add He initialization function for neural network weights

---
+++ src/tensor/winit.zig
@@ -0,0 +1,10 @@
+const NDTensor = @import("tensor.zig").NDTensor;
+const random = @import("zigrad").random;
+
+pub fn heInit(comptime T: type, tensor: *NDTensor(T)) void {
+    const fan_in: T = @floatFromInt(tensor.data.shape.shape[1]);
+    const std_dev = @sqrt(2.0 / fan_in);
+    for (tensor.data.data) |*value| {
+        value.* = random.floatNorm(T) * std_dev;
+    }
+}
