8c314eb8d5ac2c46725bcb5de8d06a983e6f3070
Marco Christiani <mchristiani2017@gmail.com>
2024-07-05 13:30:50 -0400

    feat(tensor/ops.zig): implement softmax function for normalization refactor(tensor/ops.zig): improve cross_entropy_loss with numerical stability by adding epsilon refactor(tensor/ops.zig): simplify cross_entropy_loss calculation using a single expression for loss calculation

--- src/tensor/ops.zig
+++ src/tensor/ops.zig
@@ -53,18 +53,16 @@ pub fn mse_loss(T: type, y_pred: *NDTensor(T), y: *NDTensor(T), allocator: std.m
 pub fn cross_entropy_loss(T: type, y_pred: *NDTensor(T), y: *NDTensor(T), allocator: std.mem.Allocator) !*NDTensor(T) {
     const n = @as(T, @floatFromInt(y.data.data.len));
     var sum_loss: T = 0;
+    const epsilon: T = 1e-7; // safe log
 
     for (y_pred.data.data, y.data.data) |pred, target| {
-        // assuming y is one-hot encoded, target is 0 or 1, pred is the predicted probability
-        if (target == 1) {
-            sum_loss -= @log(pred);
-        }
+        const safe_pred = @min(@max(pred, epsilon), 1 - epsilon);
+        sum_loss -= target * @log(safe_pred) + (1 - target) * @log(1 - safe_pred);
     }
-    const mean_loss = sum_loss / n;
 
+    const mean_loss = sum_loss / n;
     const result = try NDTensor(T).init(&[_]T{mean_loss}, &[_]usize{1}, true, allocator);
     result.label = "cross_entropy";
-
     try result.setChildren(@constCast(&[_]*const NDTensor(T){ y_pred, y }));
 
     result._backward = struct {
@@ -73,13 +71,13 @@ pub fn cross_entropy_loss(T: type, y_pred: *NDTensor(T), y: *NDTensor(T), alloca
             const self_children = tensor.children orelse return error.NoChildren;
             const _y_pred = self_children[0];
             const _y = self_children[1];
-
             const _n = @as(T, @floatFromInt(_y.data.data.len));
             const scale = @as(T, 1) / _n;
 
             if (_y_pred.grad) |grad| {
                 for (grad.data, _y_pred.data.data, _y.data.data) |*grad_val, pred_val, target_val| {
-                    const grad_contrib = (pred_val - target_val) * scale;
+                    const safe_pred = @min(@max(pred_val, epsilon), 1 - epsilon);
+                    const grad_contrib = (safe_pred - target_val) * scale;
                     grad_val.* += grad_contrib;
                 }
             }
@@ -87,6 +85,13 @@ pub fn cross_entropy_loss(T: type, y_pred: *NDTensor(T), y: *NDTensor(T), alloca
     }.backward;
 
     return result;
+}
+
+pub fn softmax(T: type, input: *NDTensor(T), allocator: std.mem.Allocator) !*NDTensor(T) {
+    const max_val = try input.max(allocator);
+    const exp_input = try (try input.sub(max_val, allocator)).exp(allocator);
+    const sum = try exp_input.sum(allocator);
+    return try exp_input.div(sum, allocator);
 }
 
 test "mse_loss" {
