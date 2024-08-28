db2a4e8936259a7c873d702febe072bb8e21bd34
Marco Christiani <mchristiani2017@gmail.com>
2024-08-27 22:20:43 -0400

    feat(loss.zig): add smooth_l1_loss refactor(loss.zig): rename simple_mse_loss to ag_mse_1d and ng_softmax to _softmax_fwd docs(loss.zig): yea fix(loss.zig): change requires_grad in softmax

--- src/nn/loss.zig
+++ src/nn/loss.zig
@@ -8,7 +8,8 @@ const settings = zg.settings;
 const NDTensor = zg.NDTensor;
 const GraphManager = zg.GraphManager;
 
-pub fn simple_mse_loss(T: type, y_pred: *const NDTensor(T), y: *const NDTensor(T), allocator: std.mem.Allocator) !*NDTensor(T) {
+/// Relies on autograd, operates on the flat data.
+pub fn ag_mse_1d(T: type, y_pred: *const NDTensor(T), y: *const NDTensor(T), allocator: std.mem.Allocator) !*NDTensor(T) {
     var diff = (try y_pred.sub(y, allocator)).setLabel("diff");
     const diff2 = (try NDTensor(T).init(diff.data.data, diff.data.shape.shape, true, allocator)).setLabel("diff2");
     // const diff2 = (try y_pred.sub(y, allocator)).setLabel("diff2");
@@ -56,13 +57,14 @@ pub fn mse_loss(T: type, y_pred: *const NDTensor(T), y: *const NDTensor(T), allo
     });
 }
 
+/// Runs over last dim.
 pub fn softmax_cross_entropy_loss(T: type, y_pred: *const NDTensor(T), y: *const NDTensor(T), allocator: std.mem.Allocator) !*NDTensor(T) {
     var sum_loss: T = 0;
     const epsilon: T = 1e-7;
     if (y_pred.data.shape.len() > 2) return error.NotSupported;
     const batch_size = if (y_pred.data.shape.len() > 1) try y_pred.data.shape.get(0) else 1;
     const last_dim = if (y_pred.data.shape.len() > 1) y_pred.data.shape.shape.len - 1 else 0;
-    const sm_preds = try ng_softmax(T, y_pred, last_dim, allocator);
+    const sm_preds = try _softmax_fwd(T, y_pred, last_dim, allocator);
 
     for (sm_preds.data.data, 0..) |pred, i| {
         const target = y.data.data[i];
@@ -100,6 +102,7 @@ pub fn softmax_cross_entropy_loss(T: type, y_pred: *const NDTensor(T), y: *const
     });
 }
 
+/// Relies on autograd, operates on the flat data.
 pub fn ag_softmax_1d(T: type, input: *const NDTensor(T), allocator: std.mem.Allocator) !*NDTensor(T) {
     const max_val = try input.max(allocator);
     const exp_input = try (try input.sub(max_val, allocator)).exp(allocator);
@@ -109,7 +112,7 @@ pub fn ag_softmax_1d(T: type, input: *const NDTensor(T), allocator: std.mem.Allo
 
 // There are a few ways to do this. Could SIMD sum outside the loop with an NDArray method, but accum seems like a solid idea rn.
 // mutate a view into result by directly operating on the backing ndarray
-pub fn ng_softmax(T: type, input: *const NDTensor(T), dim: usize, allocator: std.mem.Allocator) !*NDTensor(T) {
+fn _softmax_fwd(T: type, input: *const NDTensor(T), dim: usize, allocator: std.mem.Allocator) !*NDTensor(T) {
     const shape = input.data.shape.shape;
     if (dim >= shape.len) return error.InvalidDimension;
 
@@ -163,7 +166,7 @@ pub fn ng_softmax(T: type, input: *const NDTensor(T), dim: usize, allocator: std
 }
 
 pub fn softmax(T: type, input: *const NDTensor(T), dim: usize, allocator: std.mem.Allocator) !*NDTensor(T) {
-    const result = try ng_softmax(T, input, dim, allocator);
+    const result = try _softmax_fwd(T, input, dim, allocator);
     const bw_fn = struct {
         fn backward(bw_tensor: NDTensor(T), bw_allocator: std.mem.Allocator) !void {
             const bw_self_children = bw_tensor.children orelse return error.NoChildren;
@@ -202,9 +205,101 @@ pub fn softmax(T: type, input: *const NDTensor(T), dim: usize, allocator: std.me
         .data = result.data,
         .children = &[_]*const NDTensor(T){input},
         .label = "softmax",
-        .requires_grad = input.requires_grad,
         .allocator = allocator,
+        .requires_grad = true,
         ._backward = bw_fn,
         ._backward_ctx = ctx,
     });
 }
+
+pub fn smooth_l1_loss(comptime T: type, y_pred: *const NDTensor(T), y: *const NDTensor(T), beta: T, allocator: std.mem.Allocator) !*NDTensor(T) {
+    const n = @as(T, @floatFromInt(y.data.data.len));
+    var sum_loss: T = 0;
+
+    for (y_pred.data.data, y.data.data) |pred, target| {
+        const diff: T = pred - target;
+        const abs_diff: T = @abs(diff);
+        if (abs_diff < beta) {
+            sum_loss += 0.5 * (diff * diff) / beta;
+        } else {
+            sum_loss += abs_diff - (0.5 * beta);
+        }
+    }
+    const loss = sum_loss / n;
+
+    const bw_fn = struct {
+        fn backward(tensor: NDTensor(T), _allocator: std.mem.Allocator) !void {
+            const self_children = tensor.children orelse return error.NoChildren;
+            const _y_pred = self_children[0];
+            const _y = self_children[1];
+            const _bw_ctx: *T = @ptrCast(@alignCast(tensor._backward_ctx orelse return error.NoBackwardContext));
+            defer _allocator.destroy(_bw_ctx);
+            const _beta = _bw_ctx.*;
+
+            const _n = @as(T, @floatFromInt(_y.data.data.len));
+
+            if (_y_pred.grad) |grad| {
+                for (grad.data, _y_pred.data.data, _y.data.data) |*grad_val, pred_val, target_val| {
+                    const diff = pred_val - target_val;
+                    if (@abs(diff) < _beta) {
+                        grad_val.* += diff / (_beta * _n);
+                    } else {
+                        grad_val.* += std.math.sign(diff) / _n;
+                    }
+                }
+            }
+        }
+    }.backward;
+
+    const beta_ctx = try allocator.create(T);
+    beta_ctx.* = beta;
+
+    return try NDTensor(T).createDependent(.{
+        .data = try NDArray(T).init(&[_]T{loss}, &[_]usize{1}, allocator),
+        .children = &[_]*const NDTensor(T){ y_pred, y },
+        .label = "smooth_l1",
+        .requires_grad = true,
+        .allocator = allocator,
+        ._backward = bw_fn,
+        ._backward_ctx = beta_ctx,
+    });
+}
+// TODO: move this since refactor this file was renamed to loss.zig
+// pub fn gather(comptime T: type, input: *const NDTensor(T), indices: *const NDTensor(usize), dim: usize, allocator: std.mem.Allocator) !*NDTensor(T)
+
+// Documented outline for adding new custom operations
+// pub fn myop(T: type, input: *const NDTensor(T), dim: usize, allocator: std.mem.Allocator) !*NDTensor(T) {
+//     // implement forward... (could be a separate function)
+//     const bw_fn = struct {
+//         fn backward(bw_tensor: NDTensor(T), bw_allocator: std.mem.Allocator) !void {
+//             // An example of retreiving data that was saved for backward
+//             // in this example, we saved the dim (we cannot cross scope boundary in closure, so this is necessary)
+//             // We CAN reuse "T", though in the function signature since this is comptime known
+//             // Prefixing variable names with bw_ to avoid collisions with variables in the parent function scope.
+//             // We can access the operands for upstream gradients/data.
+//             const bw_self_children = bw_tensor.children orelse return error.NoChildren;
+//             const bw_input = bw_self_children[0];
+//             _ = bw_input; // autofix
+//             // We saved a *usize, so we retreive it and reify type information
+//             const bw_ctx: *usize = @ptrCast(@alignCast(bw_tensor._backward_ctx orelse return error.NoBackwardContext));
+//             const bw_dim = bw_ctx.*;
+//             _ = bw_dim; // autofix
+//             _ = bw_dim; // autofix
+//             defer bw_allocator.destroy(bw_ctx);
+//             // ...
+//         }
+//     }.backward;
+//     // save for backward (will be stored internally as a type erased pointer)
+//     const ctx = try allocator.create(usize);
+//     ctx.* = dim;
+//
+//     return try NDTensor(T).createDependent(.{
+//         .data = result, // *NDArray
+//         .children = &[_]*const NDTensor(T){input},
+//         .label = "myop",
+//         .requires_grad = input.requires_grad,
+//         .allocator = allocator,
+//         ._backward = bw_fn,
+//         ._backward_ctx = ctx,
+//     });
+// }
