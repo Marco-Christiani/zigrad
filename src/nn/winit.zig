c11af97939330aeb921715fcbcba873574542b0a
Marco Christiani <mchristiani2017@gmail.com>
2024-09-14 17:31:13 -0400

    refactor(nn/layer.zig, nn/winit.zig): replace heInit with kaimingUniformInit and biasInit feat(nn/layer.zig): add biasInit feat(nn/winit.zig): add kaimingUniformInit and biasInit

--- src/nn/winit.zig
+++ src/nn/winit.zig
@@ -1,13 +1,33 @@
+const std = @import("std");
 const zg = @import("../root.zig");
 const NDTensor = zg.NDTensor;
-// const random = zg.random;
-const std = @import("std");
 const random = std.crypto.random;
+// const random = zg.random;
 
-pub fn heInit(comptime T: type, tensor: *const NDTensor(T)) void {
-    const fan_in: T = @floatFromInt(tensor.data.shape.shape[1]);
-    const std_dev = @sqrt(2.0 / fan_in);
+pub fn kaimingUniformInit(comptime T: type, tensor: *NDTensor(T), a: T) void {
+    const fan_in = calculateFanIn(tensor);
+    const bound = @sqrt(6.0 / fan_in) * a;
+
     for (tensor.data.data) |*value| {
-        value.* = random.floatNorm(T) * std_dev;
+        value.* = random.float(T) * 2 * bound - bound;
     }
+}
+
+pub fn biasInit(comptime T: type, tensor: *NDTensor(T)) void {
+    const fan_in = calculateFanIn(tensor);
+    const bound = 1 / @sqrt(fan_in);
+
+    for (tensor.data.data) |*value| {
+        value.* = random.float(T) * 2 * bound - bound;
+    }
+}
+
+fn calculateFanIn(tensor: anytype) @TypeOf(tensor.data.data[0]) {
+    const T = @TypeOf(tensor.data.data[0]);
+    return switch (tensor.data.shape.len()) {
+        1 => @as(T, @floatFromInt(tensor.data.shape.shape[0])),
+        2 => @as(T, @floatFromInt(tensor.data.shape.shape[1])),
+        4 => @as(T, @floatFromInt(tensor.data.shape.shape[1] * tensor.data.shape.shape[2] * tensor.data.shape.shape[3])),
+        else => @panic("Unsupported tensor shape for fan_in calculation"),
+    };
 }
