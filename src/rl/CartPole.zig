79ca17c422796fa753d487f2aa15331c4b24a6af
Marco Christiani <mchristiani2017@gmail.com>
2024-09-21 01:03:04 -0400

    rl: kinematics integrator opts for cartpole

--- src/rl/CartPole.zig
+++ src/rl/CartPole.zig
@@ -2,26 +2,26 @@ const std = @import("std");
 const math = std.math;
 // const zg = @import("zigrad");
 // const T = zg.settings.precision;
-const T = f64;
+const T = f32;
 const Self = @This();
+const KinematicsIntegrator = enum { Euler, SI };
 
 gravity: T = 9.8,
 masscart: T = 1.0,
 masspole: T = 0.1,
 total_mass: T,
-length: T = 0.5, // half pole's length
+length: T = 0.5,
 polemass_length: T,
 force_mag: T = 10.0,
-tau: T = 0.02, // seconds between state updates
+tau: T = 0.02,
+kinematics_integrator: KinematicsIntegrator = .SI,
 theta_threshold_radians: T = 12.0 * 2.0 * math.pi / 360.0,
 x_threshold: T = 2.4,
 
-// State
 x: T = 0,
 x_dot: T = 0,
 theta: T = 0,
 theta_dot: T = 0,
-
 steps_beyond_done: ?i32 = null,
 rng: std.rand.DefaultPrng,
 
@@ -29,7 +29,7 @@ pub fn init(seed: u64) Self {
     var self = Self{
         .total_mass = undefined,
         .polemass_length = undefined,
-        .rng = std.rand.DefaultPrng.init(seed),
+        .rng = std.Random.DefaultPrng.init(seed),
     };
     self.total_mass = self.masscart + self.masspole;
     self.polemass_length = self.masspole * self.length;
@@ -47,42 +47,53 @@ pub fn reset(self: *Self) [4]T {
     return self.getState();
 }
 
-pub fn step(self: *Self, action: u32) struct { state: [4]T, reward: T, done: u1 } {
-    const force = if (action == 1) self.force_mag else if (action == 0) -self.force_mag else @panic("Action value out of bounds");
+pub const StepResult = struct { state: [4]T, reward: T, done: u1 };
+
+pub fn step(self: *Self, action: u32) StepResult {
+    const force = if (action == 1) self.force_mag else -self.force_mag;
     const costheta = @cos(self.theta);
     const sintheta = @sin(self.theta);
 
     const temp = (force + self.polemass_length * self.theta_dot * self.theta_dot * sintheta) / self.total_mass;
-    // const thetaacc = (self.gravity * sintheta - costheta * temp) / (self.length * (4.0 / 3.0 - self.masspole * costheta * costheta / self.total_mass));
-    const thetaacc = (self.gravity * sintheta - costheta * temp) * self.length /
-        (self.length * self.length * (4.0 / 3.0 - self.masspole * costheta * costheta / self.total_mass));
+    const thetaacc = (self.gravity * sintheta - costheta * temp) / (self.length * (4.0 / 3.0 - self.masspole * costheta * costheta / self.total_mass));
     const xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass;
 
-    self.x = self.x + self.tau * self.x_dot;
-    self.x_dot = self.x_dot + self.tau * xacc;
-    self.theta = self.theta + self.tau * self.theta_dot;
-    self.theta_dot = self.theta_dot + self.tau * thetaacc;
+    switch (self.kinematics_integrator) {
+        .Euler => {
+            self.x += self.tau * self.x_dot;
+            self.x_dot += self.tau * xacc;
+            self.theta += self.tau * self.theta_dot;
+            self.theta_dot += self.tau * thetaacc;
+        },
+        .SI => { // semi-implicit euler
+            self.x_dot += self.tau * xacc;
+            self.x += self.tau * self.x_dot;
+            self.theta_dot += self.tau * thetaacc;
+            self.theta += self.tau * self.theta_dot;
+        },
+    }
 
     const state = self.getState();
-    const done: u1 = @intFromBool(self.x < -self.x_threshold or self.x > self.x_threshold or self.theta < -self.theta_threshold_radians or self.theta > self.theta_threshold_radians);
+    const terminated: u1 = @intFromBool(self.x < -self.x_threshold or
+        self.x > self.x_threshold or
+        self.theta < -self.theta_threshold_radians or
+        self.theta > self.theta_threshold_radians);
 
     var reward: T = undefined;
-    if (done == 0) { // not done
+    if (terminated == 0) {
         reward = 1.0;
     } else if (self.steps_beyond_done == null) {
-        // pole fell
         self.steps_beyond_done = 0;
-        reward = 0.0;
+        reward = 1.0;
     } else {
         if (self.steps_beyond_done.? == 0) {
-            std.debug.print("Sim is over but you called reset. Further steps are undefined behavior.\n", .{});
+            std.debug.print("Sim is over but you called step. Further steps are undefined behavior.\n", .{});
         }
         self.steps_beyond_done.? += 1;
         reward = 0.0;
     }
 
-    // std.debug.print("[zigrad] state: {any} reward: {} done: {}\n", .{ state, reward, done });
-    return .{ .state = state, .reward = reward, .done = done };
+    return .{ .state = state, .reward = reward, .done = terminated };
 }
 
 fn getState(self: Self) [4]T {
