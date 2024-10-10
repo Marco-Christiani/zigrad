9b0f92a65e11cb4fe51862ee3e6f0328a7022df1
Marco Christiani <mchristiani2017@gmail.com>
2024-10-09 21:05:14 -0400

    tests(nn/loss): refine loss verif against torch, change precision for comparison

--- src/nn/tests/test_loss.zig
+++ src/nn/tests/test_loss.zig
@@ -8,8 +8,6 @@ const NDTensor = zg.NDTensor;
 const settings = zg.settings;
 const ag_softmax_1d = zg.loss.ag_softmax_1d;
 const softmax = zg.loss.softmax;
-const cross_entropy_loss = zg.loss.softmax_cross_entropy_loss;
-const smooth_l1_loss = zg.loss.smooth_l1_loss;
 
 const SmceTestCase = struct {
     shape: []usize,
@@ -34,33 +32,44 @@ const TestCases = struct {
     smooth_l1: SmoothL1TestCase,
 };
 
-fn runTest(comptime name: []const u8, case: SmceTestCase, allocator: std.mem.Allocator) !void {
+fn verifySmceLoss(comptime name: []const u8, case: SmceTestCase, allocator: std.mem.Allocator) !void {
+    var gm = zg.GraphManager(NDTensor(f32)).init(allocator, .{});
+    defer gm.deinit();
     const input = try NDTensor(f32).init(case.input, case.shape, true, allocator);
     defer input.deinit();
 
-    std.debug.print("{s} {d}\n", .{ name, case.shape });
-    std.debug.print("input: {d}\n", .{input.data.data});
-    std.debug.print("target: {d}\n", .{case.target});
+    std.log.info("{s} {d}\n", .{ name, case.shape });
+    std.log.info("input: {d}\n", .{input.data.data});
+    std.log.info("target: {d}\n", .{case.target});
 
     const target = try NDTensor(f32).init(case.target, case.shape, true, allocator);
     defer target.deinit();
 
-    const loss = try cross_entropy_loss(f32, input, target, allocator);
+    const loss = try zg.loss.softmax_cross_entropy_loss(f32, input, target, allocator);
     loss.grad.?.fill(1.0);
     defer loss.deinit();
-
-    try std.testing.expectApproxEqAbs(case.loss, loss.data.data[0], 1e-4);
-    var gm = zg.GraphManager(NDTensor(f32)).init(allocator, .{});
-
     try gm.backward(loss, allocator);
 
-    std.debug.print("loss: {d}\n", .{loss.data.data});
-    std.debug.print("expected: {d}\n", .{case.loss});
-    std.debug.print("input grad      : {d}\n", .{input.grad.?.data});
-    std.debug.print("expected grad   : {d}\n\n", .{case.input_grad});
+    std.log.info("loss: {d}\n", .{loss.data.data});
+    std.log.info("expected: {d}\n", .{case.loss});
+    std.log.info("input grad      : {d}\n", .{input.grad.?.data});
+    std.log.info("expected grad   : {d}\n\n", .{case.input_grad});
+
+    try std.testing.expectApproxEqAbs(case.loss, loss.data.data[0], 1e-4);
+
+    const PRECISION = f16; // precision to compare grad slices at
+    const grad_exp_trunc = try allocator.alloc(PRECISION, input.grad.?.data.len);
+    const grad_actual_trunc = try allocator.alloc(PRECISION, input.grad.?.data.len);
+    defer allocator.free(grad_exp_trunc);
+    defer allocator.free(grad_actual_trunc);
+    for (case.input_grad, input.grad.?.data, 0..) |e1, e2, i| {
+        grad_exp_trunc[i] = @floatCast(e1);
+        grad_actual_trunc[i] = @floatCast(e2);
+    }
+    try std.testing.expectEqualSlices(PRECISION, grad_exp_trunc, grad_actual_trunc);
 }
 
-test "softmax and cross-entropy tests" {
+test "softmax_cross_entropy_loss" {
     // const allocator = std.testing.allocator;
     var json_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer json_arena.deinit();
@@ -74,40 +83,40 @@ test "softmax and cross-entropy tests" {
 
     const allocator = json_arena.allocator();
     const test_cases = try json.parseFromSliceLeaky(TestCases, allocator, content, .{});
-    try runTest("softmax_crossentropy_1d", test_cases.softmax_crossentropy_1d, allocator);
-    try runTest("softmax_crossentropy_2d", test_cases.softmax_crossentropy_2d, allocator);
+    try verifySmceLoss("softmax_crossentropy_1d", test_cases.softmax_crossentropy_1d, allocator);
+    try verifySmceLoss("softmax_crossentropy_2d", test_cases.softmax_crossentropy_2d, allocator);
 }
 
-fn runSmoothL1Test(case: SmoothL1TestCase, allocator: std.mem.Allocator) !void {
+fn verifySmoothL1Loss(case: SmoothL1TestCase, allocator: std.mem.Allocator) !void {
+    var gm = zg.GraphManager(NDTensor(f32)).init(allocator, .{});
+    defer gm.deinit();
     const input = try NDTensor(f32).init(case.input, case.shape, true, allocator);
     defer input.deinit();
 
     const target = try NDTensor(f32).init(case.target, case.shape, true, allocator);
     defer target.deinit();
 
-    std.debug.print("Smooth L1 Loss Test\n", .{});
-    std.debug.print("Input: {d}\n", .{input.data.data});
-    std.debug.print("Target: {d}\n", .{target.data.data});
-    std.debug.print("Beta: {d}\n", .{case.beta});
+    std.log.info("Smooth L1 Loss Test", .{});
+    std.log.info("Input: {d}", .{input.data.data});
+    std.log.info("Target: {d}", .{target.data.data});
+    std.log.info("Beta: {d}", .{case.beta});
 
-    const loss = try smooth_l1_loss(f32, input, target, case.beta, allocator);
+    const loss = try zg.loss.smooth_l1_loss(f32, input, target, case.beta, allocator);
     loss.grad.?.fill(1.0);
     defer loss.deinit();
 
-    var gm = zg.GraphManager(NDTensor(f32)).init(allocator, .{});
-    defer gm.deinit();
-
     try gm.backward(loss, allocator);
 
-    try std.testing.expectApproxEqAbs(case.loss, loss.data.data[0], 1e-4);
+    std.log.info("Calculated loss: {d}n", .{loss.data.data[0]});
+    std.log.info("Expected loss: {d}", .{case.loss});
+    std.log.info("Calculated input grad: {d}", .{input.grad.?.data});
+    std.log.info("Expected input grad: {d}", .{case.input_grad});
 
-    std.debug.print("Calculated loss: {d}\n", .{loss.data.data[0]});
-    std.debug.print("Expected loss: {d}\n", .{case.loss});
-    std.debug.print("Calculated input grad: {d}\n", .{input.grad.?.data});
-    std.debug.print("Expected input grad: {d}\n\n", .{case.input_grad});
+    try std.testing.expectApproxEqAbs(case.loss, loss.data.data[0], 1e-4);
+    try std.testing.expectEqualSlices(f32, case.input_grad, input.grad.?.data);
 }
 
-test "smooth L1 loss" {
+test "smooth_l1_loss" {
     var json_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer json_arena.deinit();
     const file = std.fs.openFileAbsolute("/tmp/loss_test_cases.json", .{}) catch |e| {
@@ -121,5 +130,5 @@ test "smooth L1 loss" {
     const allocator = json_arena.allocator();
     const test_cases = try json.parseFromSliceLeaky(TestCases, allocator, content, .{});
 
-    try runSmoothL1Test(test_cases.smooth_l1, allocator);
+    try verifySmoothL1Loss(test_cases.smooth_l1, allocator);
 }
