ff87791cfc2390d31636c4026924ca61dc9dd218
andrewCodeDev <andrewmccommons@gmail.com>
2024-12-06 23:35:45 -0800

    fixing main for non-cuda install and adding back backend folder

--- src/main.zig
+++ src/main.zig
@@ -8,16 +8,6 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    var gpu = zg.device.CudaDevice.init(0, gpa.allocator());
-    defer gpu.deinit();
-
-    const loss = gpu.loss.nll(f32, .{
-        .dimensions = 1,
-        .input_logits = true,
-        .target_type = .index,
-        .reduce_type = .SUM,
-    });
-
-    loss.forward(...);
-    loss.backward(...);
+    var cpu = zg.device.HostDevice.init(0, gpa.allocator());
+    defer cpu.deinit();
 }
