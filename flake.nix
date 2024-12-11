ebac7af8aad337db662637a30fbdf1d470374a1a
Marco Christiani <mchristiani2017@gmail.com>
2024-12-11 08:24:06 -0500

    start flake, add mkl (zigrad build issue using nix mkl though)

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = { self, nixpkgs, zig, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+      zigpkg = zig.packages.${system}."0.13.0";
+      in
+    {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zigpkg
+            just
+            mkl
+          ];
+        };
+    }
+  );
+}
