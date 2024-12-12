be7178ae28aeb78a6e8f6b90fb773c16596a29bd
Marco Christiani <mchristiani2017@gmail.com>
2024-12-12 16:55:28 -0500

    build(flake): platform specific deps

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
+# NOTE: we use mkl but openblas also needs to be installed to provide the cblas wrapper
 {
-  description = "A very basic flake";
+  description = "Zigrad flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -17,16 +18,30 @@
         };
       };
       zigpkg = zig.packages.${system}."0.13.0";
-      in
-    {
-        devShells.default = pkgs.mkShell {
+
+      defaultShell = {
+        "aarch64-darwin" = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zigpkg
+          ];
+        };
+        "x86_64-linux" = pkgs.mkShell {
           buildInputs = with pkgs; [
             zigpkg
             mkl
-            # yes, we use mkl but this needs to be installed to provide the cblas wrapper
             openblas
           ];
         };
+        "aarch64-linux" = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zigpkg
+            openblas
+          ];
+        };
+      };
+      in
+    {
+        devShells.default = defaultShell.${system};
     }
   );
 }
