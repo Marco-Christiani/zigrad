9e36c419a0fbe7c3d70be1ae080625bd22347845
andrewCodeDev <andrewmccommons@gmail.com>
2025-06-08 17:22:06 -0700

    adding stateful optimizers and interface for generic types

--- src/zigrad.zig
+++ src/zigrad.zig
@@ -22,10 +22,21 @@ pub const conv_utils = @import("nn/conv_utils.zig");
 pub const utils = @import("nn/utils.zig");
 //pub const layer = @import("nn/layer.zig");
 pub const optim = @import("nn/optim.zig");
+pub const Optimizer = optim.Optimizer;
 pub const nn = @import("nn/nn.zig");
+
+/// The category tag is used to categorize different
+/// mathematical objects based on their mathematical
+/// category. This is used as a convenience tag to
+/// make comptime deduction easier.
+pub const Category = enum {
+    dense, // NDTensor
+    sparse, // NDSparse
+};
 
 const device_root = @import("device");
 pub const device = struct {
+    pub const Error = device_root.Error;
     pub const HostDevice = device_root.HostDevice;
     pub const CudaDevice = device_root.CudaDevice;
 };
