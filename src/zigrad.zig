26b5df332c990c5aaaf568216bfed68c222a01de
Marco Christiani <mchristiani2017@gmail.com>
2025-08-07 23:41:36 -0400

    fix(zigrad): nn export (bad merge presumably)

--- src/zigrad.zig
+++ src/zigrad.zig
@@ -24,6 +24,7 @@ pub const utils = @import("nn/utils.zig");
 pub const optim = @import("nn/optim.zig");
 pub const Optimizer = optim.Optimizer;
 pub const nn = @import("nn/nn.zig").nn;
+pub const logging = @import("logging.zig");
 
 /// The category tag is used to categorize different
 /// mathematical objects based on their mathematical
