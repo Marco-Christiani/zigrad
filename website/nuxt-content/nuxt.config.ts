342722bbaaecb6184b3ad2fae9764a0eec6bd661
Marco Christiani <mchristiani2017@gmail.com>
2026-03-09 20:43:56 -0400

    docs(website): add pipeline diagram

--- website/nuxt-content/nuxt.config.ts
+++ website/nuxt-content/nuxt.config.ts
@@ -1,5 +1,10 @@
+import svgLoader from "vite-svg-loader"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vite: {
+    plugins: [svgLoader()]
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
