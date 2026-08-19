5709c235093b0f10e825a4c3ca19c231a7e4d515
Marco Christiani <mchristiani2017@gmail.com>
2026-08-19 00:38:59 -0400

    fix(autodoc): handle hash navigation directly

--- nix/packages/zig-autodoc-docs.nix
+++ nix/packages/zig-autodoc-docs.nix
@@ -18,4 +18,5 @@ runCommand "zig-autodoc-docs-${zig.version}" {
   chmod -R u+w "$out/docs"
   patch --batch --forward --fuzz=0 --strip=1 --directory="$out/docs" < ${../patches/zig-autodoc-math.patch}
   patch --batch --forward --fuzz=0 --strip=1 --directory="$out/docs" < ${../patches/zig-autodoc-namespace-alias.patch}
+  patch --batch --forward --fuzz=0 --strip=1 --directory="$out/docs" < ${../patches/zig-autodoc-hash-navigation.patch}
 ''
